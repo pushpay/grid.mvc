959d47753c1a2e36438535f73ecffa5b8828d1b5
Phillip Hutchings <phillip.hutchings@pushpay.com>
2017-01-16 10:45:57 +1300

    Refactor number filters to use a common base

--- GridMvc/Filtering/Types/DoubleFilterType.cs
+++ GridMvc/Filtering/Types/DoubleFilterType.cs
@@ -5,34 +5,7 @@ namespace GridMvc.Filtering.Types
     /// <summary>
     ///     Object contains some logic for filtering Double columns
     /// </summary>
-    internal sealed class DoubleFilterType : FilterTypeBase
+    internal sealed class DoubleFilterType : NumberFilterType<Double>
     {
-        public override Type TargetType
-        {
-            get { return typeof (Double); }
-        }
-
-        public override GridFilterType GetValidType(GridFilterType type)
-        {
-            switch (type)
-            {
-                case GridFilterType.Equals:
-                case GridFilterType.GreaterThan:
-                case GridFilterType.LessThan:
-                case GridFilterType.GreaterThanOrEquals:
-                case GridFilterType.LessThanOrEquals:
-                    return type;
-                default:
-                    return GridFilterType.Equals;
-            }
-        }
-
-        public override object GetTypedValue(string value)
-        {
-            double db;
-            if (!double.TryParse(value, out db))
-                return null;
-            return db;
-        }
     }
 }
--- GridMvc/Filtering/Types/Int16FilterType.cs
+++ GridMvc/Filtering/Types/Int16FilterType.cs
@@ -5,34 +5,7 @@ namespace GridMvc.Filtering.Types
     /// <summary>
     ///     Object contains some logic for filtering Int32 columns
     /// </summary>
-    internal sealed class Int16FilterType : FilterTypeBase
+    internal sealed class Int16FilterType : NumberFilterType<Int16>
     {
-        public override Type TargetType
-        {
-            get { return typeof (Int16); }
-        }
-
-        public override GridFilterType GetValidType(GridFilterType type)
-        {
-            switch (type)
-            {
-                case GridFilterType.Equals:
-                case GridFilterType.GreaterThan:
-                case GridFilterType.LessThan:
-                case GridFilterType.GreaterThanOrEquals:
-                case GridFilterType.LessThanOrEquals:
-                    return type;
-                default:
-                    return GridFilterType.Equals;
-            }
-        }
-
-        public override object GetTypedValue(string value)
-        {
-            Int16 i;
-            if (!Int16.TryParse(value, out i))
-                return null;
-            return i;
-        }
     }
 }
